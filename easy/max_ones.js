53e764c341e567ca4c5bbccde8ad90b51ddc9c3f
Aaron Ludwin <aludwin1@binghamton.edu>
2018-07-28 18:51:14 -0400

    completed findMaxConsecutiveOnes

---
+++ easy/max_ones.js
@@ -0,0 +1,10 @@
+var findMaxConsecutiveOnes = function(nums) {
+  let max = 0;
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+      if(nums[i] === 1) count++;
+      else count = 0;
+      if(count > max) max = count;
+  }
+  return max;
+};
