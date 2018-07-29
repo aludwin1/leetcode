08672345a3112b87abafb2576b3517ed7af6f853
Aaron Ludwin <aludwin1@binghamton.edu>
2018-07-29 15:12:08 -0400

    made solution far more concise

--- medium/find_bottom_left_tree_val.js
+++ medium/find_bottom_left_tree_val.js
@@ -1,17 +1,10 @@
 const findBottomLeftValue = root => {
-  let nodes = [root];
-  let allNodes = [];
-  while (nodes.length > 0) {
-      let currentNode = nodes.shift();
-      if(currentNode.right) {
-          nodes.push(currentNode.right);
-          allNodes.push(currentNode.right);
-      }
-      if(currentNode.left) {
-          nodes.push(currentNode.left);
-          allNodes.push(currentNode.left);
-      }
+  const allNodes = [root];
+  let lastNode;
+  while (allNodes.length > 0) {
+      lastNode = allNodes.shift();
+      if(lastNode.right) allNodes.push(lastNode.right);
+      if(lastNode.left) allNodes.push(lastNode.left);
   }
-  if(allNodes.length === 0) return root.val;
-  else return allNodes.pop().val;
+  return lastNode.val;
 };
